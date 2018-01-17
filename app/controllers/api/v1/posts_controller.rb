6757cfec834d5d0c681068781c9f2b73da441d55
Jessica Chaves <Jessica@Jessicas-MacBook-Air.local>
2018-01-17 21:48:09 -0200

    create other methods

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,6 +5,41 @@ module Api
         posts = Post.all.order('created_at desc')
         render json: {status: 'success', data: posts}
       end
+
+      def show
+        post = Post.find(params[:id])
+        render json: {status: 200, message: 'Loaded post', data: post}
+      end
+
+      def create
+        post = Post.create(post_params)
+        render json: render_message(post, :create)
+      end
+
+      def destroy
+        post = Post.find(params[:id]).destroy
+        render json: render_message(post, :destroy)
+      end
+
+      private
+
+      def post_params
+        params.permit(:title, :body)
+      end
+
+      def render_message(post, action)
+        messages = {
+          true: { status: 200, message: "Post is #{action}", data: post },
+          false: { status: 401, message: "Post is not #{action}", data: post.errors.messages }
+        }
+
+        actions = {
+          create:  post.persisted?.to_s.to_sym,
+          destroy: post.destroyed?.to_s.to_sym
+        }
+
+        messages[actions[action]]
+      end
     end
   end
 end
