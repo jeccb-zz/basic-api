84da381ce6b8671b7ad4c52464c449a14598bf89
Jessica Barbosa <jbarbosa@bionexo.com>
2018-01-22 14:34:16 -0200

    refactoring all methods

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,42 +3,28 @@ module Api
     class PostsController < ApplicationController
       def index
         posts = Post.all.order('created_at desc')
-        render json: {status: 'success', data: posts}
+        render json: { data: posts }, status: :ok
       end
 
       def show
         post = Post.find(params[:id])
-        render json: {status: 200, message: 'Loaded post', data: post}
+        render json: { data: post },status: :ok
       end
 
       def create
         post = Post.create(post_params)
-        render json: render_message(post, :create)
+        render json: { data: post },status: :ok
       end
 
       def destroy
         post = Post.find(params[:id]).destroy
-        render json: render_message(post, :destroy)
+        render json: { data: post },status: :ok
       end
 
       private
 
       def post_params
         params.permit(:title, :body)
-      end
-
-      def render_message(post, action)
-        messages = {
-          true: { status: 200, message: "Post is #{action}", data: post },
-          false: { status: 401, message: "Post is not #{action}", data: post.errors.messages }
-        }
-
-        actions = {
-          create:  post.persisted?.to_s.to_sym,
-          destroy: post.destroyed?.to_s.to_sym
-        }
-
-        messages[actions[action]]
       end
     end
   end
